63e31d782ae6fcc4e68035a243b7ec1e421f3135
John Davison <johncdavison@gmail.com>
2014-08-10 10:09:56 -0700

    Snap, = is not ==

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require './models'
 SECRET_VOLCANO_TOKEN = ENV["SECRET_VOLCANO_TOKEN"]
 
 get("/balance") do
-  if params[:token] = SECRET_VOLCANO_TOKEN
+  if params[:token] == SECRET_VOLCANO_TOKEN
     Balance.last.to_json
   else
     "you are not authorized"
